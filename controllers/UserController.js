f51115fca73f655c0645b3eef79a9dba056d5d04
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-02 14:42:42 -0300

    feat: add validations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,8 +3,25 @@ class UserController {
   }
 
   async create (req, res, next) {
-    console.log(req.body);
-    res.send('Pegando o corpo da requisição');
+    let { name, email, password } = req.body;
+
+    if (!name || name === undefined || name.length < 3) {
+      res.status(400);
+      res.json({ "err": "Nome inválido" });
+    } 
+
+    if (!email || email === undefined) {
+      res.status(400);
+      res.json({ "err": "E-mail inválido" });
+    } 
+
+    if (!password || password === undefined) {
+      res.status(400);
+      res.json({ "err": "Senha inválida"});
+    }
+
+    res.status(200);
+    res.json({ "status": "Usuário cadastrado com sucesso" });
   }
 }
 
