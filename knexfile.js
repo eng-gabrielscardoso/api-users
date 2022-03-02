5bf6beb486ca6999d2c3ff228d81fdd02a5a023a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-02 14:17:42 -0300

    feat: preparing knex migrations

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+require('dotenv-safe').config();
+
+module.exports = {
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  migrations: {
+    directory: __dirname + '/database/migrations',
+  },
+  seeds: {
+    directory: __dirname + '/database/seeds',
+  },
+}
