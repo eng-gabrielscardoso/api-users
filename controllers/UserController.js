756fff959daedc35f2785a63c18ab09a27cf7671
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-05 22:26:52 -0300

    feat: add validations and new methods
    add method to create a new user
    add valitations to prevent register a user with duplicated e-mail

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,16 +9,27 @@ class UserController {
     if (!name || name === undefined || name.length < 3) {
       res.status(400);
       res.json({ "err": "Nome inválido" });
+      return;
     } 
 
     if (!email || email === undefined) {
       res.status(400);
       res.json({ "err": "E-mail inválido" });
+      return;
     } 
 
     if (!password || password === undefined) {
       res.status(400);
       res.json({ "err": "Senha inválida"});
+      return;
+    }
+
+    const emailExists = await User.findEmail(email);
+
+    if (emailExists) {
+      res.status(406)
+      res.json({ "err": "E-mail já está cadastrado no banco de dados" });
+      return;
     }
 
     await User.create(name, email, password);
--- models/User.js
+++ models/User.js
@@ -5,12 +5,33 @@ const knex = require('../database/connection');
 class User {
   async create (name, email, password) {
     try {
+      const hash = await bcrypt.hash(password, 10)
+
       await knex
-        .insert({ name, email, password, role: 0 })
+        .insert({ name, email, password: hash, role: 0 })
         .table('users');
     } catch (e) {
       console.error(e);
     }
+  }
+
+  async findEmail (email) {
+    try {
+      const result = await knex
+        .select('*')
+        .from('users')
+        .where({ email: email, })
+
+      if (result.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+
   }
 }
 
